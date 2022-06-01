b67ad950552b852cc3b2340d4fdd32c1addbbf51
ted <82011813+unnotedme@users.noreply.github.com>
2022-06-01 16:11:43 -0300

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# docs
+
+in this project, i provide study materials produced by me and links to help you study programming.
+
+## license
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ script.js
@@ -0,0 +1,13 @@
+// dark mode
+
+const toggle = document.getElementById("toggle");
+const theme = window.localStorage.getItem("theme");
+
+if (theme === "dark") document.body.classList.add("dark");
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    if (theme === "dark") {
+        window.localStorage.setItem("theme", "light");
+    } else window.localStorage.setItem("theme", "dark");
+});
